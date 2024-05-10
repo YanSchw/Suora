a695fdad891e5043d8e597437ada2f339ccc6499
YanSchw <schoessow.yannick@gmail.com>
2024-05-10 10:29:14 +0200

    Changed Website

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<p align="center">
-  <a href="https://github.com/YanSchw/SuoraEngine">
-    <img src="https://github.com/YanSchw/SuoraEngine/blob/main/Content/EngineContent/SuoraLogo.png?raw=true" width="400" alt="Suora Engine logo">
-  </a>
-</p>
+![SuoraEngine](img/suora.png)
---
+++ index.js
@@ -0,0 +1,44 @@
+"use strict";
+
+console.log('Here is an Easter Egg 🥚');
+
+/*
+    type animation in first section
+*/
+const dynamicText = document.querySelector("#animated-text");
+const words = ["Game Engine", "Open Source", "C++ ❤"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking");
+    if (!isDeleting && charIndex < currentWord.length) {
+        charIndex++;
+        setTimeout(typeEffect, 100);
+    } else if (isDeleting && charIndex > 0) {
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else {
+        isDeleting = !isDeleting;
+        dynamicText.classList.remove("stop-blinking");
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+typeEffect();
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+const hiddenElement = document.querySelectorAll('.hide');
+hiddenElement.forEach((element) => observer.observe(element));
